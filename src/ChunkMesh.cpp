29e82a4de0ff0d999e2c43a42c64f08c381ab031
ilumary <christoph.britsch@icloud.com>
2022-02-06 20:56:13 +0100

    fixed bugs with wrong orientation of faces

--- src/ChunkMesh.cpp
+++ src/ChunkMesh.cpp
@@ -5,7 +5,7 @@ glm::vec4 ChunkMesh::calculate_uv(int x, int y, int size) {
 	return glm::vec4(step * x, step * y, step * x + step, step * y + step);
 }
 
-void ChunkMesh::generate(Chunk* chunk, World* world) {
+void ChunkMesh::generate(Chunk& chunk, World* world) {
     vertices.clear();
 	indices.clear();
 
@@ -13,19 +13,19 @@ void ChunkMesh::generate(Chunk* chunk, World* world) {
 
 	uint32_t index_constants[6] = {0, 3, 2, 2, 1, 0};
 
-	glm::ivec3 start = { chunk->chunk_position_.x * Chunk::SIZE, chunk->chunk_position_.y * Chunk::SIZE, chunk->chunk_position_.z * Chunk::SIZE };
+	glm::ivec3 start = { chunk.chunk_position_.x * Chunk::SIZE, chunk.chunk_position_.y * Chunk::SIZE, chunk.chunk_position_.z * Chunk::SIZE };
 
 	for (int x = 0; x < Chunk::SIZE; ++x) {
 		for (int z = 0; z < Chunk::SIZE; ++z) {
 			for (int y = 0; y < Chunk::SIZE; ++y) {
-				int value = chunk->chunk_data_[x][y][z];
+				int value = chunk.chunk_data_[x][y][z];
 
 				if (value != Chunk::BlockType::CLEAR) {
-					//invert y and z values because standard vulcan viewport is flipped 
+					//invert y because standard vulcan viewport is flipped 
 					float ix = x + start.x;
 					float iy = (y * -1) - start.y;
-					float iz = (z * -1) + start.z;
-						
+					float iz = z + start.z;
+					
 					float px = 1.0f + ix;
 					float py = iy - 1.0f;
 					float pz = 1.0f + iz;
@@ -35,29 +35,29 @@ void ChunkMesh::generate(Chunk* chunk, World* world) {
 					float nz = iz;
 					
 					//Back Face
-					if (z == Chunk::SIZE - 1 || chunk->chunk_data_[x][y][z + 1] == Chunk::BlockType::CLEAR) {
+					if (z == 0 || chunk.chunk_data_[x][y][z - 1] == Chunk::BlockType::CLEAR) {
 						add_block_side_indices(index_constants, size);
 						
-						vertices.push_back({ { nx, ny, nz },{ 0, 0, -1 },{ block_uvs[value - 1].x, block_uvs[value - 1].w } });
-						vertices.push_back({ { nx, py, nz },{ 0, 0, -1 },{ block_uvs[value - 1].x, block_uvs[value - 1].y } });
-						vertices.push_back({ { px, py, nz },{ 0, 0, -1 },{ block_uvs[value - 1].z, block_uvs[value - 1].y } });
-						vertices.push_back({ { px, ny, nz },{ 0, 0, -1 },{ block_uvs[value - 1].z, block_uvs[value - 1].w } });
+						vertices.push_back({ { nx, ny, nz },{ 0, 0, 1 },{ block_uvs[value - 1].x, block_uvs[value - 1].w } });
+						vertices.push_back({ { nx, py, nz },{ 0, 0, 1 },{ block_uvs[value - 1].x, block_uvs[value - 1].y } });
+						vertices.push_back({ { px, py, nz },{ 0, 0, 1 },{ block_uvs[value - 1].z, block_uvs[value - 1].y } });
+						vertices.push_back({ { px, ny, nz },{ 0, 0, 1 },{ block_uvs[value - 1].z, block_uvs[value - 1].w } });
 						size += 4;
 					}
 
 					//Front Face
-					if (z == 0 || chunk->chunk_data_[x][y][z - 1] == Chunk::BlockType::CLEAR) {
+					if (z == Chunk::SIZE - 1 || chunk.chunk_data_[x][y][z + 1] == Chunk::BlockType::CLEAR) {
 						add_block_side_indices(index_constants, size);
 
-						vertices.push_back({ { px, ny, pz },{ 0, 0, 1 },{ block_uvs[value - 1].x, block_uvs[value - 1].w } });
-						vertices.push_back({ { px, py, pz },{ 0, 0, 1 },{ block_uvs[value - 1].x, block_uvs[value - 1].y } });
-						vertices.push_back({ { nx, py, pz },{ 0, 0, 1 },{ block_uvs[value - 1].z, block_uvs[value - 1].y } });
-						vertices.push_back({ { nx, ny, pz },{ 0, 0, 1 },{ block_uvs[value - 1].z, block_uvs[value - 1].w } });
+						vertices.push_back({ { px, ny, pz },{ 0, 0, -1 },{ block_uvs[value - 1].x, block_uvs[value - 1].w } });
+						vertices.push_back({ { px, py, pz },{ 0, 0, -1 },{ block_uvs[value - 1].x, block_uvs[value - 1].y } });
+						vertices.push_back({ { nx, py, pz },{ 0, 0, -1 },{ block_uvs[value - 1].z, block_uvs[value - 1].y } });
+						vertices.push_back({ { nx, ny, pz },{ 0, 0, -1 },{ block_uvs[value - 1].z, block_uvs[value - 1].w } });
 						size += 4;
 					}
 
 					//Top face
-					if (y == Chunk::SIZE - 1 || chunk->chunk_data_[x][y + 1][z] == Chunk::BlockType::CLEAR) {
+					if (y == Chunk::SIZE - 1 || chunk.chunk_data_[x][y + 1][z] == Chunk::BlockType::CLEAR) {
 						add_block_side_indices(index_constants, size);
 
 						vertices.push_back({ { nx, py, nz },{ 0, -1, 0 },{ block_uvs[value - 1].x, block_uvs[value - 1].w } });
@@ -67,8 +67,8 @@ void ChunkMesh::generate(Chunk* chunk, World* world) {
 						size += 4;
 					}
 
-					//Top face
-					if (y == 0 || chunk->chunk_data_[x][y - 1][z] == Chunk::BlockType::CLEAR) {
+					//Bottom face
+					if (y == 0 || chunk.chunk_data_[x][y - 1][z] == Chunk::BlockType::CLEAR) {
 						add_block_side_indices(index_constants, size);
 
 						vertices.push_back({ { nx, ny, pz },{ 0, 1, 0 },{ block_uvs[value - 1].x, block_uvs[value - 1].w } });
@@ -79,7 +79,7 @@ void ChunkMesh::generate(Chunk* chunk, World* world) {
 					}
 
 					//Right face
-					if (x == Chunk::SIZE - 1 || chunk->chunk_data_[x + 1][y][z] == Chunk::BlockType::CLEAR) {
+					if (x == Chunk::SIZE - 1 || chunk.chunk_data_[x + 1][y][z] == Chunk::BlockType::CLEAR) {
 						add_block_side_indices(index_constants, size);
 
 						vertices.push_back({ { px, ny, nz },{ 1, 0, 0 },{ block_uvs[value - 1].x, block_uvs[value - 1].w } });
@@ -90,7 +90,7 @@ void ChunkMesh::generate(Chunk* chunk, World* world) {
 					}
 
 					//Left face
-					if (x == 0 || chunk->chunk_data_[x - 1][y][z] == Chunk::BlockType::CLEAR) {
+					if (x == 0 || chunk.chunk_data_[x - 1][y][z] == Chunk::BlockType::CLEAR) {
 						add_block_side_indices(index_constants, size);
 
 						vertices.push_back({ { nx, ny, pz },{ -1, 0, 0 },{ block_uvs[value - 1].x, block_uvs[value - 1].w } });
