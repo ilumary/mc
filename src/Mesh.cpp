5854c9a97f2d3667cd5df641732b55b4e6628fde
ilumary <christoph.britsch@icloud.com>
2022-02-06 20:56:41 +0100

    fixed bug with wrong indice calculation

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -8,17 +8,22 @@ void Mesh::destroy(vkc::Core& core) {
 }
 
 void Mesh::merge(const Mesh* other) {
-    std::cout << "Merging Mesh with " << other->vertices.size() << " other vertices." << std::endl;
+    //take vertex count before vertice merge to prevent corruption of first element
+    uint32_t vertex_count = vertices.size();
+
+    //std::cout << "Merging Mesh with " << other->vertices.size() << " into Mesh with "<< vertices.size() << " vertices" << std::endl;
 
     vertices.insert(vertices.end(), other->vertices.begin(), other->vertices.end());
 
-    std::cout << "Successfully merged vertices " << std::endl;
-
     std::vector<uint32_t> tmp = other->indices;
-    uint32_t vertex_count = vertices.size();
+
+    //std::cout << "Merging " << tmp.size() << " indices onto " << vertices.size() << " vertices" << std::endl;
+
     for(std::size_t i = 0; i < tmp.size(); ++i) {
         tmp[i] += vertex_count;
     }
 
     indices.insert(indices.end(), tmp.begin(), tmp.end());
+
+    //std::cout << "Mesh now containing " << vertices.size() << " vertices and " << indices.size() << " indices" << std::endl;
 }
